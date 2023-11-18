f2b89ce9ad6e528233c81e6caddb1d5e9b283950
Nina Sánchez <128436086+JaninaSz@users.noreply.github.com>
2023-11-18 06:53:39 -0300

    code

--- scripts/resul.js
+++ scripts/resul.js
@@ -1,33 +1,33 @@
 
-
-//EstadisticasFijas
-var entTbpd=document.getElementById("e-tbpd").value;
-var entTnpd=document.getElementById("e-tnpd").value;
-var entTtpd=document.getElementById("e-ttpd").value;
+//*EstadisticasFijas
+var entTbla=document.getElementById("e-tbla").value;
+var entTnul=document.getElementById("e-tnul").value;
+var entTvot=document.getElementById("e-tvot").value;
 //Mesas escrutadas
 var entEscrut=document.getElementById("e-cantReg").value;
 
 //calcular el porcentaje de participacion, sobre el tot de electores
-document.getElementById("por-bla").textContent = entTbpd;
-document.getElementById("por-anu").textContent = entTnpd;
+document.getElementById("por-bla").textContent = entTbla;
+document.getElementById("por-anu").textContent = entTnul;
 
-document.getElementById("tolvo").textContent = entTtpd;
+document.getElementById("tolvo").textContent = entTvot;
 document.getElementById("escrut").textContent = entEscrut;
 
 //Porcentaje participacion
 //total 5 circuitos: 34.064 personas 
 totalpersonas=34064
-document.getElementById("tolvo").textContent = entTtpd;
+//document.getElementById("tolvo").textContent = entTtpd;
 //Mesas escrutadas
 totalmesas=101
-votaron=25600
-partici=(votaron/totalpersonas)*100;
+votaron=1600
+partici=(entTvot/totalpersonas)*100;
 porParti= Number(partici.toFixed(2));
 alert(porParti);
 document.getElementById("pf-partici").textContent = porParti;
-document.getElementById("por-par").textContent = 75,15;
+document.getElementById("por-par").textContent = porParti;
 
 function actualizar(){
+   
     var estado =document.getElementById("dv-load").style.display;
   if(estado=="none"){
       document.getElementById("dv-load").style.display="flex";
@@ -35,7 +35,4 @@ function actualizar(){
       document.getElementById("dv-load").style.display="none";        
   }  
 }
-
-
-
 
